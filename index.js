50f6a492b11a9d1aad77a47bb4b95021f7b6410f
SAMUEL KOBINA GYASI <gyasisamuelkobina22@gmail.com>
2022-07-19 05:05:55 +0100

    portofolio init15

--- index.js
+++ index.js
@@ -24,3 +24,21 @@ function closemenu() {
     sideMenu.style.right = "-200px"
 }
 
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxFmbSIUHteaUssiOKDx_uWZgzJSxMB9JiISpsK4V_w4CWiGO95tD7DaI0JReXknEMi/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message Sent"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
